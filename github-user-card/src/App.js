860436cf851ffe55d3f18b744351288266662712
Michael-B1764 <mikebailar@gmail.com>
2020-03-10 13:01:56 -0700

    got my data to show in the dom. now to get followers data

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,14 +1,38 @@
 import React from 'react'
-
+import axios from "axios"
 import './App.css'
 import Card from "./components/Card"
 
-function App() {
-  return (
-    <div className="App">
-      <Card />
-    </div>
-  );
+
+class App extends React.Component {
+
+  state= {
+    myData: {},
+    usersData:{}
+  }
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users/michael-bailar")
+    .then(res => {
+      console.log("myres", res)
+      this.setState({
+        myData: res.data
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  render() {
+    // console.log("my state", this.state.myData)
+    // console.log("users state", this.state.usersData)
+    return (
+      <div className="App">
+        <Card myData={this.state.myData}/>
+      </div>
+    );
+  }
 }
 
 export default App
--- github-user-card/src/components/Card.js
+++ github-user-card/src/components/Card.js
@@ -1,9 +1,14 @@
 import React from "react"
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <p>test</p>
+        <div className="my-data-card">
+            <img className="profile-img" src={props.myData.avatar_url} alt="users profile image"/>
+            <h1>Michael Bailar</h1>
+            <h2>Github Tag: {props.myData.login}</h2>
+        </div>
     )
+    
 }
 
 export default Card
